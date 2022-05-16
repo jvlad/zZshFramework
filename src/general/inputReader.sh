5d8cbd8e7f02ee1b28c67a09739b953ce3bc0306
Dex Ho <dexHo@mobileAppSoft.com>
2022-05-16 06:23:44 +0200

    Added semantic-based exit-status to

--- src/general/inputReader.sh
+++ src/general/inputReader.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env zsh
 
 argsOrPipeIn-args$(zsf)() {
-  local input=$(if isEmpty:String ${@} ;then \
+  local input
+  input=$(if isEmpty:String ${@} ;then \
         read -r -d '' -t 0.01 inputPipe
         print$(zsf) "${inputPipe}"
+        return 1
     else
         print$(zsf) ${@}
+        return 0
     fi)
+  local e=$?
   print$(zsf) "${input}"
+  return $e
 }
 
 pipeInOrArgs-args$(zsf)() {
