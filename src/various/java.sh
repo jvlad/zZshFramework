8504a91d4f487daf1731e2cf4c6255c5f45f7f14
Vlad Zams <v+pub@vladzams.com>
2023-10-26 22:57:47 -0400

    Added selection of default node LTS version matching the one supported by AWS; fixed java-related commands

--- src/various/java.sh
+++ src/various/java.sh
@@ -1,7 +1,11 @@
 
+javaHomePathPrint$(zsf)() {
+  /usr/libexec/java_home
+}
+
 javaHomeCopyPathToClipboard() {
   if isCommandExist-command java ;then
-    sysClipboardCopyVerbose-args ${JAVA_HOME}
+    sysClipboardCopyVerbose-args $(javaHomePathPrint$(zsf))
   else
     print-errorMessage "java not installed"
     return $(error$(zsf))
