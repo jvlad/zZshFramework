604cce352ec9d6291230d256d6312d55189fd204
Vlad Zams <pub@vladzams.com>
2023-12-06 20:25:00 -0500

    Backup ###### Wed Dec  6 20:25:00 EST 2023

--- src/various/shell.sh
+++ src/various/shell.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env zsh
-
-_main_shell-sourceDir() {
-  # debugLogFunc-args$(z39) {$@}
-  local srcDir="$1"
   
-  isLastCommandSucceed$(z39)() {
-    if [[ $? -eq 0 ]] ;then
-      return $(yes$(z39))
-    else
-      return $(no$(z39))
-    fi
-  }
-  
+isLastCommandSucceed$(z39)() {
+  if [[ $? -eq 0 ]] ;then
+    return $(yes$(z39))
+  else
+    return $(no$(z39))
+  fi
 }
-_callAndForget-function-args _main_shell-sourceDir $(dirname $0)
+
+shellGoto-dir() {
+  cd "$1"
+}
+
+shellGotoPreviousDir() {
+    cd ~-
+}
